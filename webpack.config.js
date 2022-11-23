5582997d36c1470a443d6c185b9ba789ece0bad0
Fred Brennan <copypaste@kittens.ph>
2022-11-23 18:48:53 -0500

    Fix running from local

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     }),
   ],
   devServer: {
-    contentBase: path.join(__dirname, 'build'),
+    contentBase: path.join(__dirname, 'src'),
     compress: true,
     port: 8080,
   },
